5bd6a56c9c8014d7248ed0f69dd4d6aefd5bf489
bc94 <Bojan.Cvjekovic@student.uibk.ac.atgit config --global user.email Bojan.Cvjetkovic@student.uibk.ac.at>
2015-05-15 17:09:59 +0200

    Added wall and floor

--- Merry_Go_Round/MerryGoRound.cpp
+++ Merry_Go_Round/MerryGoRound.cpp
@@ -194,27 +194,6 @@ void Mouse(int button, int state, int x, int y)
             camera.back(0.1);
             break;
     }
-
-    if(state == GLUT_DOWN)
-    {
-      /* Depending on button pressed, set rotation axis,
-       * turn on animation */
-        switch(button)
-	{
-	    case GLUT_LEFT_BUTTON:
-	        axis = Xaxis;
-		break;
-
-	    case GLUT_MIDDLE_BUTTON:
-  	        axis = Yaxis;
-	        break;
-
-	    case GLUT_RIGHT_BUTTON:
-	        axis = Zaxis;
-		break;
-	}
-	anim = GL_TRUE;
-    }
 }
 
 
