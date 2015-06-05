ac70bd95469ef282180e7ea97bbdd69f090e9fce
bc94 <Bojan.Cvjekovic@student.uibk.ac.atgit config --global user.email Bojan.Cvjetkovic@student.uibk.ac.at>
2015-06-05 12:18:27 +0200

    Added hsv manipulation per keyboard

--- Merry_Go_Round/source/Lightsource.h
+++ Merry_Go_Round/source/Lightsource.h
@@ -9,17 +9,23 @@
 
 #ifndef __LIGHTSOURCE_H__
 	#define __LIGHTSOURCE_H__
+	#define GLM_FORCE_RADIANS
 	
 	#include <GL/glut.h>
 	#include <GL/gl.h>
+	#include "../glm/glm.hpp"
+	#include "../glm/gtc/matrix_transform.hpp"
+	#include "../glm/gtc/type_ptr.hpp"
 	
 	class Lightsource {
 		public:
-			Lightsource(double x_offset, double y_offset, double z_offset, float color_r, float color_g, float color_b);
+			Lightsource(float x_offset, float y_offset, float z_offset, float color_r, float color_g, float color_b);
 			~Lightsource();
-			GLfloat* vertex_buffer_data;
-			GLfloat* color_buffer_data;
+			glm::vec3 pos;
+			glm::vec3 rgb;
 			GLushort* index_buffer_data;
+			glm::vec3 rgbToHsv(glm::vec3 rgbVal);
+			glm::vec3 hsvToRgb(glm::vec3 hsvVal);
 	};
 	
 #endif
